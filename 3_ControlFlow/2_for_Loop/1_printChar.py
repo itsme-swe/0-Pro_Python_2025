5e44c9438e011209cc640ba009b8c4e12f388877
itsme-swe <me.harshmehra07@gmail.com>
2024-10-14 17:11:38 +0530

    update

---
+++ 3_ControlFlow/2_for_Loop/1_printChar.py
@@ -0,0 +1,7 @@
+# Print the characters using for loop
+
+name = "Harsh"
+for ele in name:
+  print(ele, end=" ")
+
+  # output ⇒  H a r s h
---
+++ 3_ControlFlow/2_for_Loop/2_rangeFunction.py
@@ -0,0 +1,6 @@
+# Print table using range function
+
+n = int(input("Enter the value of n:"))
+
+for ele in range(1,11):
+  print(n, '*', ele, '=', 5 * ele)
